80a6c50f3f51b0875be42480efda8a6e5cf4c9e2
Bruno Cardoso <bruno@liveonsolutions.com>
2022-04-21 19:52:28 -0300

    finish login function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function send() {
+function usernamePasswordValidatior() {
   const username = document.querySelector("input[name='username']").value;
   const password = document.querySelector("input[name='password']").value;
 
@@ -15,6 +15,28 @@ function send() {
   };
 }
 
+function login() {
+  const usernamePasswordValidation = usernamePasswordValidatior();
+
+  if (usernamePasswordValidation.success) {
+    const userFund = table.find(function (row) {
+      if (row.username == usernamePasswordValidation.username) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (!userFund || userFund.password != usernamePasswordValidation.password) {
+      alert("O usuário e senha informados não podem ser encontrados!");
+
+      return;
+    }
+
+    alert("LoBoi");
+  }
+}
+
 function register() {
   const email = document.querySelector('input[name="email"]').value;
   const fullname = document.querySelector('input[name="fullname"]').value;
@@ -22,10 +44,12 @@ function register() {
   if (email.length < 6 || fullname.length < 6) {
     alert("Preenche essa porra direito, seu animal!");
 
-    return;
+    return {
+      success: false,
+    };
   }
 
-  const usernamePasswordValidation = send();
+  const usernamePasswordValidation = usernamePasswordValidatior();
 
   if (usernamePasswordValidation.success) {
     const data = {
@@ -36,13 +60,31 @@ function register() {
     };
 
     console.log(database(data));
+
+    return {
+      success: true,
+      data: data,
+    };
   }
 }
 
-const table = [];
+const table = [
+  {
+    email: "bruno@liveon.com",
+    fullname: "bruno",
+    username: "brunindela",
+    password: "123456",
+  },
+  {
+    email: "",
+    fullname: "",
+    username: "",
+    password: "",
+  },
+];
 
-function database(data) {
-  table.push(data);
+function database(argument) {
+  table.push(argument);
 
   return table;
 }
